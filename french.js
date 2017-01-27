e9a8c0dec93d1fc60e0d95f894a7ba32bc248258
Matt Seaton <matt.r.seaton@gmail.com>
2017-01-27 15:09:28 -0700

    arrays complete

---
+++ french.js
@@ -0,0 +1,4 @@
+const phrases = [
+  'Tu ressembles à un sac de patates!', 'Bonne bonté!', 'Boule de fromage', 'Dites "fromage!"', 'Le gros fromage', 'Le fromage est comme le fromage', 'Fromage à lui',
+  "Le fromage est bon pour l'âme", "Qu'arrive-t-il au trou quand le fromage est parti?", "La vie est belle. Le fromage le rend meilleur."
+];
